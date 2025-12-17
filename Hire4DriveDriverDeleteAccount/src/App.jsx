c976326975a2621f6f61e40a29eced4b3b9d97b6
DhruvPopat268 <dhruvpopat268@gmail.com>
2025-12-17 13:44:50 +0530

    updated code to use hash router

--- Hire4DriveDriverDeleteAccount/src/App.jsx
+++ Hire4DriveDriverDeleteAccount/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { Toaster, toast } from 'react-hot-toast'
 import './App.css'
 
